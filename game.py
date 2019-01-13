61c7da2b0b47b47883b77800a149ac0e1f15872d
jhw <hw_jia628@163.com>
2019-01-13 23:36:04 +0800

    666 master

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ this is 33 on pc
 this is 444 on pc
 this is 4 on github
 this is 5 on github -- dev_test
-this is 6 on pc
+this is 666 on pc
 this is 6666 on github
 this is 7777 on github
 this is 888 on github
