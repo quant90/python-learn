5c71e494c92d18d579d972e233e4efb1c26f54f9
jhw <hw_jia628@163.com>
2019-01-13 23:28:35 +0800

    6666 7777 master

--- game.py
+++ game.py
@@ -8,6 +8,6 @@ this is 444 on pc
 this is 4 on github
 this is 5 on github -- dev_test
 this is 6 on pc
-this is 66 on github
-this is 77 on github
+this is 6666 on github
+this is 7777 on github
 this is 888 on github
